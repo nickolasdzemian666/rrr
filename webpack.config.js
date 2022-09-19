ded8cc13487ad323057ce148e66c7ba0935bae6e
nickolasdzemian <9999775303@mail.ru>
2022-09-20 01:30:42 +0300

    R4
    R4

--- webpack.config.js
+++ webpack.config.js
@@ -3,116 +3,119 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const BundleAnalyzerPlugin =
-    require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const withReport = process.env.npm_config_withReport;
 
 module.exports = {
-    devServer: {
+  devServer: {
     client: {
-        logging: 'info',
+      logging: 'info',
     },
     compress: true,
     historyApiFallback: true,
     port: 8000,
-    },
-    devtool:
-        process.env.NODE_ENV === 'production'
-        ? 'hidden-source-map'
-        : 'eval-source-map',
-    entry: path.resolve(__dirname, './src/index.jsx'),
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    module: {
+  },
+  devtool:
+    process.env.NODE_ENV === 'production'
+      ? 'hidden-source-map'
+      : 'eval-source-map',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  module: {
     rules: [
-        {
+      {
         exclude: /node_modules/,
-        test: /\.jsx?$/,
+        test: /\.(t|j)sx?$/,
         use: ['babel-loader'],
-        },
-        {
+      },
+      // { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
         exclude: /\.module\.s?css$/i,
         test: /\.s?css$/i,
         use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
             loader: 'css-loader',
             options: {
-                modules: {
+              modules: {
                 localIdentName: '[name]___[hash:base64:5]',
                 mode: 'icss',
-                    },
-                },
+              },
             },
-            'sass-loader',
+          },
+          'sass-loader',
         ],
-        },
-        {
+      },
+      {
         test: /\.module\.s?css$/,
         use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
+          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
             loader: 'css-loader',
             options: {
-                modules: {
+              modules: {
                 localIdentName: '[name]___[hash:base64:5]',
                 mode: 'local',
-                },
+              },
             },
-            },
-            'sass-loader',
+          },
+          'sass-loader',
         ],
-        },
-        {
+      },
+      {
         generator: {
-            filename: 'static/[hash][ext]',
+          filename: 'static/[hash][ext]',
         },
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        },
+      },
       // {
       //   loader: 'html-loader',
       //   test: /\.html$/i,
       // },
     ],
-    },
-    optimization: {
+  },
+  optimization: {
     minimizer: ['...', new CssMinimizerPlugin()],
-    },
-    output: {
+  },
+  output: {
     clean: true,
     environment: {
-        arrowFunction: false,
+      arrowFunction: false,
     },
     filename: '[name].bundle.[chunkhash].js',
     path: path.resolve(__dirname, './build'),
-    },
-    performance: {
+  },
+  performance: {
     hints: false,
-        maxAssetSize: 512000,
-        maxEntrypointSize: 512000,
-    },
-    plugins: [
+    maxAssetSize: 512000,
+    maxEntrypointSize: 512000,
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, './public/index.html'),
+      template: path.resolve(__dirname, './public/index.html'),
     }),
     ...(isDev
-        ? [new MiniCssExtractPlugin()]
-        : [
-            new MiniCssExtractPlugin({
+      ? [new MiniCssExtractPlugin()]
+      : [
+          new MiniCssExtractPlugin({
             chunkFilename: '[name].[contenthash].css',
             filename: '[name].[contenthash].css',
-            }),
+          }),
         ]),
     ...(withReport ? new BundleAnalyzerPlugin() : ''),
-        ],
-    resolve: {
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+  resolve: {
     alias: {
-        components: path.resolve(__dirname, 'src/components/'),
-        src: path.resolve(__dirname, 'src'),
-        store: path.resolve(__dirname, 'src/store'),
-        svg: path.resolve(__dirname, 'src/assets/svg'),
+      components: path.resolve(__dirname, 'src/components/'),
+      src: path.resolve(__dirname, 'src'),
+      store: path.resolve(__dirname, 'src/store'),
+      svg: path.resolve(__dirname, 'src/assets/svg'),
     },
-    extensions: ['.jsx', '.js'],
-    },
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
 };
